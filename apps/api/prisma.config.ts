03968dcc8a0d67239eff64b997e734c8fcdea1b4
Plamen Ratchev <plamen@ratchev.net>
2026-02-12 22:29:09 -0500

    Fix for databse URL in env

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 
 export default defineConfig({
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
+  ...(process.env.DATABASE_URL
+    ? { datasource: { url: process.env.DATABASE_URL } }
+    : {}),
 });
